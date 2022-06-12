751c9a2c5204b25cd2d01af5cb181ac0c05ee62b
Caitano <josiasmartinscaitano@gmail.com>
2022-06-12 19:25:43 -0300

    feat(2.5): utlizando o pipe do rxjs

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Animais } from '../animais';
 import { UsuarioService } from '../../autenticacao/usuario/usuario.service';
 import { AnimaisService } from '../animais.service';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-animais',
@@ -9,8 +11,7 @@ import { AnimaisService } from '../animais.service';
   styleUrls: ['./lista-animais.component.css']
 })
 export class ListaAnimaisComponent implements OnInit {
-
-  animais!: Animais;
+  animais$!: Observable<Animais>;
 
   constructor(
     private usuarioService: UsuarioService,
@@ -18,12 +19,21 @@ export class ListaAnimaisComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario().subscribe((usuario) => {
-      const userNme = usuario.name ?? '';
-      this.animaisService.listaDoUsuario(userNme).subscribe((animais) => {
-        this.animais = animais;
-      });
-    })
+    // this.usuarioService.retornaUsuario().subscribe((usuario) => {
+    //   const userNme = usuario.name ?? '';
+    //   this.animaisService.listaDoUsuario(userNme).subscribe((animais) => {
+    //     this.animais = animais;
+    //   });
+    // })
+    //outra forma de fazer (melhore prática)
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      // switchMap: é um Observer troca o fluxo
+      // recebe como parametro, o retorno do primeiro observer
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
+      })
+    );
   }
 
 }
