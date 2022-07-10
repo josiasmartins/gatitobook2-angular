2369769882ef2139df7880821845eb4763ce3f84
Caitano <josiasmartinscaitano@gmail.com>
2022-07-10 13:02:52 -0300

    feat(3.07): adicioando o métodos de curtir e excluir foto

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { Animais, Animal } from './animais';
 import { TokenService } from '../autenticacao/token.service';
 import { environment } from '../../environments/environment';
+import { catchError, mapTo } from 'rxjs/operators';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root'
@@ -28,5 +30,21 @@ export class AnimaisService {
     // const token = this.tokenService.retornaToken();
     // const headers = new HttpHeaders().append('x-access-token', token);
     return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluirAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: boolean): Observable<boolean> {
+    // pipe: manipular o fluxo da requisicao;
+    // terceiro parametro do post - { oberserve: 'response' }: retorna o status da requisição
+    return this.http.post(`${API}/photos/${id}/likes`, {}, { observe: 'response' }).pipe(
+      // mapTo: emite o valor passado por parametro
+      mapTo(true),
+      catchError(error => {
+        return error.status == NOT_MODIFIED ? of() : throwError(error);
+      })
+    )
   }
 }
