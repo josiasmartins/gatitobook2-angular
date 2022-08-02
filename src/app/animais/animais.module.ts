d421274caa6eca63642350389b8527fb550a9c2b
Caitano <josiasmartinscaitano@gmail.com>
2022-08-02 20:21:02 -0300

    feat: adiconado o template

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -7,15 +7,24 @@ import { AnimalComponent } from './animal/animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
-import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios/comentarios.component';
-
+import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent, ComentariosComponent],
+  declarations: [
+    ListaAnimaisComponent,
+    AnimalComponent,
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent,
+    ComentariosComponent
+  ],
   imports: [
     CommonModule,
     AnimaisRoutingModule,
-    CartaoModule
+    CartaoModule,
+    MensagemModule,
+    ReactiveFormsModule
   ]
 })
 export class AnimaisModule { }
